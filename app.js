0869c69cff67337a028750038c7d8073955f5a3e
Dheeraj <dk1563@gmail.com>
2025-03-16 18:59:38 +0530

    Handle CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'; // Import cors package
 import userRoutes from './routes/users.js';
 import eventRoutes from './routes/events.js'; // Import event routes
 import { initializeDatabase } from './database.js';
@@ -6,7 +7,10 @@ import { initializeDatabase } from './database.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()); // Use cors middleware
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/users', userRoutes);
 app.use('/events', eventRoutes); // Use event routes
 
